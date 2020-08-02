1001eef92789b0f482c47eabe1363288ab90d4a9
Chris-xcc <chris_guoc@163.com>
2020-08-02 18:56:12 +0800

    __init__

---
+++ src/plugins/element.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import {
+  Main,
+  Aside,
+  Header,
+  Footer,
+  Container,
+  Menu,
+  MenuItem,
+  Submenu,
+  Button,
+  Image,
+  Form,
+  FormItem,
+  Input,
+  Carousel,
+  CarouselItem,
+  Switch,
+} from 'element-ui';
+
+Vue.use(Main);
+Vue.use(Aside);
+Vue.use(Header);
+Vue.use(Footer);
+Vue.use(Container);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Submenu);
+Vue.use(Button);
+Vue.use(Image);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Switch);
