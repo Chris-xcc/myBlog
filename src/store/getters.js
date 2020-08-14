7de772628fdaf81df48c83ded275032ef3b74e4d
Chris-xcc <chris_guoc@163.com>
2020-08-14 21:55:21 +0800

    add vuex config

---
+++ src/store/getters.js
@@ -0,0 +1,8 @@
+export default {
+    //获取token方法
+    //判断是否有token,如果没有重新赋值，返回给state的token
+    getToken(state){
+        return state.token
+    }
+
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
+
+
 Vue.use(Vuex)
 
+const state = {
+    token: '', //初始化token
+}
 
 const store = new Vuex.Store({
-  state: {
-    token: '' //初始化token
-  },
-  mutations: {
-    //存储token方法
-    //设置token等于外部传递进来的值
-    setToken(state, token) {
-      state.token = token
-      localStorage.token = token //同步存储token至localStorage
-    },
-
-  },
-  getters: {
-    //获取token方法
-    //判断是否有token,如果没有重新赋值，返回给state的token
-    getToken(state) {
-      if (!state.token) {
-        state.token = localStorage.getItem('token')
-      }
-      return state.token
-    }
-  },
-
-  actions: {},
-  modules: {}
+    state,
+    mutations,
+    actions,
+    getters,
+    modules: {}
 })
 export default store;
