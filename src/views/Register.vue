df59f1e189d494dd9ed185b79561e3eda2504b44
Chris-xcc <chris_guoc@163.com>
2020-08-16 22:16:01 +0800

    views register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,49 +1,59 @@
 <template>
   <home>
-    <el-form :model="registerForm" ref="registerFormRef" status-icon label-width="100px">
-      <el-form-item>
-        <el-input
-          type="text"
-          v-model="registerForm.email"
-          autocomplete="on"
-          prefix-icon="el-icon-message"
-          placeholder="邮箱"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input
-          type="text"
-          v-model="registerForm.username"
-          autocomplete="on"
-          prefix-icon="el-icon-s-custom"
-          placeholder="用户名"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input
-          type="password"
-          v-model="registerForm.password"
-          autocomplete="off"
-          prefix-icon="el-icon-lock"
-          placeholder="密码"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input
-          type="password"
-          v-model="registerForm.check_password"
-          autocomplete="off"
-          prefix-icon="el-icon-key"
-          placeholder="确认密码"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="register('registerFormRef')" style="width:100%">注册</el-button>
-      </el-form-item>
-    </el-form>
-    <router-link to="login" >
-      <div class="login">登录</div>
-    </router-link>
+    <div class="register">
+      <div class="container">
+                <div class="top">注册</div>
+
+        <el-form :model="registerForm" ref="registerFormRef" status-icon>
+          <el-form-item>
+            <el-input
+              type="text"
+              v-model="registerForm.email"
+              autocomplete="on"
+              prefix-icon="el-icon-message"
+              placeholder="邮箱"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="text"
+              v-model="registerForm.username"
+              autocomplete="on"
+              prefix-icon="el-icon-s-custom"
+              placeholder="用户名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              v-model="registerForm.password"
+              autocomplete="off"
+              prefix-icon="el-icon-lock"
+              placeholder="密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              v-model="registerForm.check_password"
+              autocomplete="off"
+              prefix-icon="el-icon-key"
+              placeholder="确认密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="register('registerFormRef')" style="width:100%">注册</el-button>
+          </el-form-item>
+        </el-form>
+        <div class="bottom-box">
+          <div class="right">
+            <router-link to="login" tag="div">
+              <div class="login">登录</div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </home>
 </template>
 
@@ -83,5 +93,10 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+@import url("@/assets/css/login/login.less");
+
+.login {
+  cursor: pointer;
+}
 </style>
