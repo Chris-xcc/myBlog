426b428d61be446cb77f4191066dd591725b2784
Chris-xcc <chris_guoc@163.com>
2020-08-20 03:03:57 +0800

    format

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,23 +1,23 @@
 import {
-    SET_TOKEN,
-    REMOVE_TOKEN, Get_Page
+  SET_TOKEN,
+  REMOVE_TOKEN,
+  Get_Page,
 } from "./mutations-types";
 
-export default
-{
-    //存储token方法
-    //设置token等于外部传递进来的值
-    [SET_TOKEN](state,payload){
-        state.token = payload.token
-        // console.log(payload);
-    },
+export default {
+  //存储token方法
+  //设置token等于外部传递进来的值
+  [SET_TOKEN](state, payload) {
+    state.token = payload.token
+    // console.log(payload);
+  },
 
-    [REMOVE_TOKEN](state){
-        state.token = ''
-        // console.log(payload.token);
-    },
-    [Get_Page](state,payload){
-        // console.log(payload)
-        state.page=window.localStorage.getItem('page')
-    }
-  }
+  [REMOVE_TOKEN](state) {
+    state.token = ''
+    // console.log(payload.token);
+  },
+  [Get_Page](state, payload) {
+    // console.log(payload)
+    state.page = window.localStorage.getItem('page')
+  },
+}
